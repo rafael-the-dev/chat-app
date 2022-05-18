54628c51ffb94e7d379171aa78e45468ebc032da
Rafael Tivane <luislangabusiness@gmail.com>
2022-05-18 11:59:39 +0200

    define tabs and listen to click events in the footer tabs button

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import { IconButton, Paper } from '@mui/material'
+import { IconButton } from '@mui/material'
 import HomeIcon from '@mui/icons-material/Home';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import NotificationsIcon from '@mui/icons-material/Notifications';
@@ -7,29 +7,32 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import classes from './styles.module.css'
 import { library } from '@fortawesome/fontawesome-svg-core';
-//import fontawesome from '@fortawesome/fontawesome'
-//import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import { faComment } from '@fortawesome/free-solid-svg-icons'
-
+import { useRouter } from 'next/router'
 library.add(faComment);
 
 const Footer = () => {
+    const router = useRouter();
+    const { pathname } = router;
+
+    const clickHandler = prop => () => router.push(`${pathname}${prop ? `?tab=${prop}` : "" }`);
+
     return (
         <footer className={classNames(`bottom-0 fixed w-full`)}>
             <nav className={classNames(classes.navbar, "bg-white flex justify-between py-2 px-2 w-full")}>
-                <IconButton>
+                <IconButton onClick={clickHandler()}>
                     <HomeIcon className={classNames("text-3xl text-red-500")} />
                 </IconButton>
-                <IconButton>
+                <IconButton onClick={clickHandler("friends")}>
                     <PeopleAltIcon className={classNames("text-3xl text-cyan-500")} />
                 </IconButton>
-                <IconButton>
+                <IconButton onClick={clickHandler("chat")}>
                     <FontAwesomeIcon className={classNames("text-2xl text-cyan-500")} icon="fa-solid comment fa-comment" />
                 </IconButton>
-                <IconButton>
+                <IconButton onClick={clickHandler("notifications")}>
                     <NotificationsIcon className={classNames("text-3xl text-cyan-500")} />
                 </IconButton>
-                <IconButton>
+                <IconButton onClick={clickHandler("settings")}>
                     <SettingsIcon className={classNames("text-3xl text-cyan-500")} />
                 </IconButton>
             </nav>
