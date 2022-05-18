50204028b2026a0ae1d8a9b9e5aa6168a0a4a7c0
Rafael Tivane <luislangabusiness@gmail.com>
2022-05-18 09:09:03 +0200

    customize footer navbar

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,29 +1,30 @@
 import classNames from 'classnames'
-import { IconButton } from '@mui/material'
+import { IconButton, Paper } from '@mui/material'
 import HomeIcon from '@mui/icons-material/Home';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import classes from './styles.module.css'
 
 const Footer = () => {
     return (
-        <footer className={classNames(`bottom-0 fixed flex justify-between w-full`)}>
-            <nav>
+        <footer className={classNames(`bottom-0 fixed w-full`)}>
+            <nav className={classNames(classes.navbar, "bg-white flex justify-between py-2 px-2 w-full")}>
                 <IconButton>
-                    <HomeIcon />
+                    <HomeIcon className={classNames("text-3xl text-red-500")} />
                 </IconButton>
                 <IconButton>
-                    <FontAwesomeIcon icon="fa-solid fa-comment" />
+                    <FontAwesomeIcon className={classNames("text-3xl text-cyan-500")} icon="fa-solid fa-comment" />
                 </IconButton>
                 <IconButton>
-                    <PeopleAltIcon />
+                    <PeopleAltIcon className={classNames("text-3xl text-cyan-500")} />
                 </IconButton>
                 <IconButton>
-                    <NotificationsIcon />
+                    <NotificationsIcon className={classNames("text-3xl text-cyan-500")} />
                 </IconButton>
                 <IconButton>
-                    <SettingsIcon />
+                    <SettingsIcon className={classNames("text-3xl text-cyan-500")} />
                 </IconButton>
             </nav>
         </footer>
