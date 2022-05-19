e9af3fb906a6c205ac60eedad30656f46be5fd7a
Rafael Tivane <luislangabusiness@gmail.com>
2022-05-19 11:38:26 +0200

    add search form

--- src/components/Friendship/components/search-friends/index.jsx
+++ src/components/Friendship/components/search-friends/index.jsx
@@ -1,5 +1,9 @@
 import { useContext } from 'react'
+import { IconButton } from '@mui/material'
 import classNames from 'classnames';
+
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import SearchIcon from '@mui/icons-material/Search';
 //import { LoginContext } from 'src/context/LoginContext';
 import { AppContext } from "src/context/AppContext";
 
@@ -10,6 +14,18 @@ const Container = ({ className }) => {
 
     return (
         <div className={classNames(className)}>
+            <form className={classNames("border border-solid border-slate-200 flex items-center px-2 py-1")}>
+                <IconButton>
+                    <FilterAltIcon />
+                </IconButton>
+                <input 
+                    className={classNames("border-0 grow text-base outline-none py-3")}
+                    placeholder="search username"
+                />
+                <IconButton>
+                    <SearchIcon />
+                </IconButton>
+            </form>
             <div className="px-5 pt-6">
                 {
                     getUsersList()?.map((item, index) => <FriendCard key={index} { ...item } />)
