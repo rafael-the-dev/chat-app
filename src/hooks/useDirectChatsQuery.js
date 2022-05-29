cae87efbdf63be447f3bfe5037db08296e742e19
Rafael Tivane <luislangabusiness@gmail.com>
2022-05-29 08:47:24 +0200

    use lazy query in getDirectChat hook

--- src/hooks/useDirectChatsQuery.js
+++ src/hooks/useDirectChatsQuery.js
@@ -1,50 +1,53 @@
-import { useEffect, useMemo, useState } from 'react'
-import { useQuery, useSubscription } from "@apollo/client"
-
+import { useContext, useEffect, useMemo, useState } from 'react'
+import { useLazyQuery, useSubscription } from "@apollo/client"
+import { LoginContext } from "src/context/LoginContext"
 
 import { GET_DIRECTS_CHAT } from 'src/graphql/queries';
 import { DIRECT_MESSAGE_SENT } from 'src/graphql/subscriptions';
 
 export const useDirectChatsQuery = ({ loggedUser }) => {
-    const users = useMemo(() => loggedUser ? [ loggedUser.username, loggedUser.username ] : [], [ loggedUser ]);
+    const { user } = useContext(LoginContext);
+    const users = useMemo(() => user ? [ user.username, user.username ] : [], [ user ]);
 
     const subscription = useSubscription(DIRECT_MESSAGE_SENT, { variables: { users } });
-    const { subscribeToMore, ...result } = useQuery(GET_DIRECTS_CHAT);
+    const [ getDirectChats, { data, loading, error, subscribeToMore } ] = useLazyQuery(GET_DIRECTS_CHAT);
 
-    const [ data, setData ] = useState(null);
-    const [ loading, setLoading ] = useState(false);
-    const [ error, setError ] = useState(null);
+    //const [ data, setData ] = useState(null);
+    //const [ loading, setLoading ] = useState(false);
+    //const [ error, setError ] = useState(null);
 
     useEffect(() => {
-        subscribeToMore({
-            document: DIRECT_MESSAGE_SENT,
-            variables: { users },
-            updateQuery: (prev, { subscriptionData }) => {
-                if (!subscriptionData.data || !Boolean(loggedUser)) return prev;
+        if(user) {
+            subscribeToMore({
+                document: DIRECT_MESSAGE_SENT,
+                variables: { users },
+                updateQuery: (prev, { subscriptionData }) => {
+                    if (!subscriptionData.data) return prev;
 
-                const chats = [ ...prev.directChats ]
-                const receivedChat = subscriptionData.data.messageSent;
+                    const chats = [ ...prev.directChats ]
+                    const receivedChat = subscriptionData.data.messageSent;
 
-                const chatIndex = chats.findIndex(item => item.ID === receivedChat.ID);
+                    const chatIndex = chats.findIndex(item => item.ID === receivedChat.ID);
 
-                if(chatIndex !== -1) {
-                    chats[chatIndex] = receivedChat;
+                    if(chatIndex !== -1) {
+                        chats[chatIndex] = receivedChat;
 
-                    return Object.assign({}, prev, {
-                        directChats: [...chats ]
-                    });
+                        return Object.assign({}, prev, {
+                            directChats: [...chats ]
+                        });
+                    }
+
+                    return prev;
                 }
-
-                return prev;
-            }
-        });
-    }, [ loggedUser, subscribeToMore, users ]); 
+            });
+        }
+    }, [ subscribeToMore, user, users ]); 
   
     useEffect(() => {
-        if(result.data) {
-            setData(result.data);
+        if(user) {
+            getDirectChats()//setData(result.data);
         }
-    }, [ result ]);
+    }, [ getDirectChats, user ]);
   
     return { data, loading, error };
 };
