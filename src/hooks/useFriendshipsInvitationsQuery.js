623c3d70c5deade87a76c3c3d4a3613cdf6319a9
Rafael Tivane <luislangabusiness@gmail.com>
2022-05-29 08:05:30 +0200

    use lazy queries in friendships invitation and friendships list

--- src/hooks/useFriendshipsInvitationsQuery.js
+++ src/hooks/useFriendshipsInvitationsQuery.js
@@ -1,41 +1,45 @@
-import { useEffect, useMemo, useState } from 'react'
-import { useQuery, useSubscription } from "@apollo/client"
+import { useContext, useEffect, useMemo, useState } from 'react'
+import { useLazyQuery, useSubscription } from "@apollo/client"
+import { LoginContext } from "src/context/LoginContext"
 
 import { GET_FRIENDSHIPS_INVITATIONS } from 'src/graphql/queries';
 import { FRIENDSHIP_INVITATION_SENT } from 'src/graphql/subscriptions';
 
 export const useFriendshipsInvitationsQuery = (loggedUser) => {
+    const { user } = useContext(LoginContext);
     const username = useMemo(() => loggedUser ? loggedUser.username : "", [ loggedUser ]);
     const subscription = useSubscription(FRIENDSHIP_INVITATION_SENT, { variables: { id: username }})
     
-    const { subscribeToMore, ...result } = useQuery(GET_FRIENDSHIPS_INVITATIONS);
+    const [ getFriendshipsInvitations, { data, loading, error, subscribeToMore } ]  = useLazyQuery(GET_FRIENDSHIPS_INVITATIONS);
 
-    const [ data, setData ] = useState(null);
-    const [ loading, setLoading ] = useState(false);
-    const [ error, setError ] = useState(null);
+    //const [ data, setData ] = useState(null);
+    //const [ loading, setLoading ] = useState(false);
+    //const [ error, setError ] = useState(null);
 
     useEffect(() => {
-        subscribeToMore({
-            document: FRIENDSHIP_INVITATION_SENT,
-            variables: { id: username },
-            updateQuery: (prev, { subscriptionData }) => {
-                if (!subscriptionData.data || !Boolean(loggedUser)) return prev;
+        if(Boolean(user)) {
+            subscribeToMore({
+                document: FRIENDSHIP_INVITATION_SENT,
+                variables: { id: username },
+                updateQuery: (prev, { subscriptionData }) => {
+                    if (!subscriptionData.data || !Boolean(user)) return prev;
 
-                const invitation = subscriptionData.data.friendshipInvitationSent;
-                let friendshipInvitations = [ invitation, ...prev.friendshipInvitations.filter(item => item.sender.username !== invitation.sender.username) ];
-                
-                return Object.assign({}, prev, {
-                    friendshipInvitations
-                });
-            }
-        });
-    }, [ loggedUser, subscribeToMore, username ]); 
+                    const invitation = subscriptionData.data.friendshipInvitationSent;
+                    let friendshipInvitations = [ invitation, ...prev.friendshipInvitations.filter(item => item.sender.username !== invitation.sender.username) ];
+                    
+                    return Object.assign({}, prev, {
+                        friendshipInvitations
+                    });
+                }
+            });
+        }
+    }, [ user, subscribeToMore, username ]); 
   
     useEffect(() => {
-        if(result.data) {
-            setData(result.data);
+        if(user) {
+            getFriendshipsInvitations()//setData(result.data);
         }
-    }, [ result ]);
+    }, [ getFriendshipsInvitations, user ]);
   
     return { data, loading, error };
 };
--- src/hooks/useFriendshipsQuery.js
+++ src/hooks/useFriendshipsQuery.js
@@ -1,45 +1,53 @@
-import { useEffect, useMemo, useState } from 'react'
-import { useQuery, useSubscription } from "@apollo/client"
-
+import { useContext, useEffect, useMemo, useState } from 'react'
+import { useQuery, useLazyQuery, useSubscription } from "@apollo/client"
+import { LoginContext } from "src/context/LoginContext"
 
 import { GET_FRIENDSHIPS } from 'src/graphql/queries';
 import { FRIENDSHIP_INVITATION_ACCEPTED } from 'src/graphql/subscriptions';
 
 export const useFriendshipsQuery = (loggedUser) => {
+    const { user } = useContext(LoginContext);
     const username = useMemo(() => loggedUser ? loggedUser.username : "", [ loggedUser ]);
 
     const subscription = useSubscription(FRIENDSHIP_INVITATION_ACCEPTED, { variables: { id: username }})
-    const { subscribeToMore, ...result } = useQuery(GET_FRIENDSHIPS);
+    const [ getFriendshipsList, { data, loading, error, subscribeToMore } ] = useLazyQuery(GET_FRIENDSHIPS)
+    //const { subscribeToMore, ...result } = useQuery(GET_FRIENDSHIPS);
 
-    const [ data, setData ] = useState(null);
-    const [ loading, setLoading ] = useState(false);
-    const [ error, setError ] = useState(null);
+    //const [ data, setData ] = useState(null);
+    //const [ loading, setLoading ] = useState(false);
+    //const [ error, setError ] = useState(null);
+
+    //console.log(result)
 
     useEffect(() => {
-        subscribeToMore({
-            document: FRIENDSHIP_INVITATION_ACCEPTED,
-            variables: { id: username },
-            updateQuery: (prev, { subscriptionData }) => {
-                if (!subscriptionData.data || !Boolean(loggedUser)) return prev;
+        if(Boolean(user)) {
+            subscribeToMore({
+                document: FRIENDSHIP_INVITATION_ACCEPTED,
+                variables: { id: username },
+                updateQuery: (prev, { subscriptionData }) => {
+                    if (!subscriptionData.data || !Boolean(user)) return prev;
 
-                const friendshipStatus = subscriptionData.data.friendshipInvitationAccepted;
-                const friendships = [ ...prev.friendships ];
+                    const friendshipStatus = subscriptionData.data.friendshipInvitationAccepted;
+                    const friendships = [ ...prev.friendships ];
 
-                if(loggedUser.username === friendshipStatus.sender.username) friendships.push(friendshipStatus.receiver);
-                else friendships.push(friendshipStatus.sender);
+                    if(user.username === friendshipStatus.sender.username) friendships.push(friendshipStatus.receiver);
+                    else friendships.push(friendshipStatus.sender);
 
-                return Object.assign({}, prev, {
-                    friendships
-                });
-            }
-        });
-    }, [ loggedUser, subscribeToMore, username ]); 
+                    return Object.assign({}, prev, {
+                        friendships
+                    });
+                }
+            });
+        }
+    }, [ user, subscribeToMore, username ]); 
+
+    
   
     useEffect(() => {
-        if(result.data) {
-            setData(result.data);
+        if(Boolean(user)) {
+            //getFriendshipsList()
         }
-    }, [ result ]);
+    }, [ getFriendshipsList, user ]);
   
     return { data, loading, error };
 };
