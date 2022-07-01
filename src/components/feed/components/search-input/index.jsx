a3b116659b0158cdb3cdac6b11f206f28d2b2ae7
Rafael Tivane <luislangabusiness@gmail.com>
2022-07-01 20:50:54 +0200

    fix input transition bugs

--- src/components/feed/components/search-input/index.jsx
+++ src/components/feed/components/search-input/index.jsx
@@ -1,7 +1,8 @@
 import { useCallback, useState } from "react"
 import { IconButton } from "@mui/material"
 import classNames from "classnames"
-import styles from "./styles.module.css"
+import { useRouter } from "next/router"
+import classes from "./styles.module.css"
 
 import SearchIcon from '@mui/icons-material/Search';
 import CloseIcon from '@mui/icons-material/Close';
@@ -9,12 +10,17 @@ import CloseIcon from '@mui/icons-material/Close';
 const SearchInput = () => {
     const [ open, setOpen ] = useState(false);
 
-    const toggleState = useCallback(() => setOpen(b => !b), [])//rounded-2xl
+    const router = useRouter();
+    const { pathname } = router;
 
+    const toggleState = useCallback(() => setOpen(b => !b), [])//rounded-2xl
+    
     return (
-        <div className={classNames(styles.transition,"bg-transition bg-slate-300 flex items-center rounded-full dark:bg-stone-400", { "grow pl-3 pr-1": open })}>
+        <div 
+            className={classNames(classes.transition, { "max-w-sm": pathname === "/"},
+            "bg-slate-300 flex items-center justify-end rounded-full dark:bg-stone-400", { "grow pl-3 pr-1": open })}>
             <input 
-                className={classNames(styles.transition, "bg-transparent border-0 outline-none h-0 w-0", { "h-6 grow": open }, { "hidden": !open })} 
+                className={classNames(classes.transition, "bg-transparent border-0 outline-none h-0 w-0", { "h-6 grow w-auto": open }, { "p-0": !open })} 
                 placeholder="search..."
             />
             <IconButton
